bd68f11f1fbe7b64be575d282180de39dd92e9cb
Ikramul-m <i20727172@gmail.com>
2024-05-03 15:35:01 +0530

    admin

--- MERN-Ecommerce-Website/src/App.tsx
+++ MERN-Ecommerce-Website/src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import { lazy } from "react"
-
+import { lazy, Suspense } from "react"
+import Loader from "./Components/loader"
 
 const Home = lazy(() => import("./pages/Home"))
 const Search = lazy(() => import("./pages/Search"))
 const Cart = lazy(() => import("./pages/Cart"))
 
+
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Search" element={<Search />} />
-        <Route path="/Cart" element={<Cart />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Search" element={<Search />} />
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
+      </Suspense>
+
     </Router>
   )
 }
