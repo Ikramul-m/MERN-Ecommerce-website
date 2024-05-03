6c04ce967512489e81086aca8508a7477da872cf
Ikramul-m <i20727172@gmail.com>
2024-05-03 15:02:41 +0530

    Routes

--- MERN-Ecommerce-Website/src/App.tsx
+++ MERN-Ecommerce-Website/src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { lazy } from "react"
+
+
+const Home = lazy(() => import("./pages/Home"))
+const Search = lazy(() => import("./pages/Search"))
+const Cart = lazy(() => import("./pages/Cart"))
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route>
-
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/Search" element={<Search />} />
+        <Route path="/Cart" element={<Cart />} />
       </Routes>
     </Router>
   )
