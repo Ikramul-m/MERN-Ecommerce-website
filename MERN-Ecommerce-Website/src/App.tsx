9b2d533139d5523cf5f627c333caefc48c0165cc
Ikramul-m <i20727172@gmail.com>
2024-05-03 16:44:39 +0530

    Admin

--- MERN-Ecommerce-Website/src/App.tsx
+++ MERN-Ecommerce-Website/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { lazy, Suspense } from "react"
-import Loader from "./Components/loader"
+import Loader from "./Components/admin/Loader"
 
 const Home = lazy(() => import("./pages/Home"))
 const Search = lazy(() => import("./pages/Search"))
