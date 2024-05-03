4db93516a213fd11d78fbf8d0d146b4ac140960b
Ikramul-m <i20727172@gmail.com>
2024-05-03 14:41:21 +0530

    App

--- MERN-Ecommerce-Website/src/App.tsx
+++ MERN-Ecommerce-Website/src/App.tsx
@@ -2,9 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
+    <Router>
+      <Routes>
+        <Route>
 
-    </div>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
